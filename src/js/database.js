9cec186cb8bb44d959494b094078d651219740a3
EdmondPW <64176781+EdmondPW@users.noreply.github.com>
2020-06-24 16:59:06 +0700

    Update database.js

--- src/js/database.js
+++ src/js/database.js
@@ -1,1 +1,58 @@
 
+var firebaseConfig = {
+          apiKey: "AIzaSyABQG0uk7MI0upKCVegOMF_3bjIwvxDQgM",
+          authDomain: "resepsaver.firebaseapp.com",
+          databaseURL: "https://resepsaver.firebaseio.com",
+          projectId: "resepsaver",
+          storageBucket: "resepsaver.appspot.com",
+          messagingSenderId: "104620766512",
+          appId: "1:104620766512:web:47b12acaedf67edff144f5"
+        };
+
+        //usahakan initialize firebase jadi sebuah function
+        // Initialize Firebase
+        firebase.initializeApp(firebaseConfig);
+        firebase.auth.Auth.Persistence.LOCAL;
+
+        const auth=firebase.auth();
+
+         function cekIfLoginOrNot(){
+            firebase.auth().onAuthStateChanged(function(res){
+            if(res){
+              alert("Welcome")
+            }
+            else{
+              alert("You must sign in")
+              window.location.href="./login";
+            }
+           })
+         }
+
+        function signup(){
+          
+        }
+
+        function signin(){
+          //alert("tes");
+          var email=document.getElementById("email");
+          var password=document.getElementById("pass");
+
+          const promise=auth.signInWithEmailAndPassword(email.value,password.value);
+          promise.catch(function(err){
+                                  alert("Gagal Login");
+                                  });
+          auth.onAuthStateChanged(function(res){
+              if(res){
+                  window.location.href="index.html";
+              }
+          })
+        }
+
+        function saveRecipie(){
+          
+        }
+
+        function unSaveRecipe(){
+          
+        }
+        
