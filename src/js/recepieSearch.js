f7b89aad4b3c5044c7eda0ee53bc6797bfa19039
EdmondPW <64176781+EdmondPW@users.noreply.github.com>
2020-06-24 17:11:28 +0700

    Update recepieSearch.js

--- src/js/recepieSearch.js
+++ src/js/recepieSearch.js
@@ -1,18 +1,33 @@
 $( "#inputSearch" ).click(function() {
     var searchInput = document.getElementById("Search_Recipe").value;
     alert(searchInput);
+    
     var settings = {
-      "async": true,
-      "crossDomain": true,
-      "url": "https://edamam-food-and-grocery-database.p.rapidapi.com/parser?ingr="+searchInput,
-      "method": "GET",
-      "headers": {
-        "x-rapidapi-host": "edamam-food-and-grocery-database.p.rapidapi.com",
-        "x-rapidapi-key": "ed79f9f0b7msh65c44d70d45c322p1f8f63jsne93f7593dc3d"
-      }
+        "async": true,
+        "crossDomain": true,
+        "url": "https://edamam-recipe-search.p.rapidapi.com/search?q="+searchInput,
+        "method": "GET",
+        "headers": {
+            "x-rapidapi-host": "edamam-recipe-search.p.rapidapi.com",
+            "x-rapidapi-key": "ed79f9f0b7msh65c44d70d45c322p1f8f63jsne93f7593dc3d"
+        }
     }
-  
+
     $.ajax(settings).done(function (response) {
-      console.log(response);
+        console.log(response);
     });
+    //var settings = {
+      //"async": true,
+      //"crossDomain": true,
+      //"url": "https://edamam-food-and-grocery-database.p.rapidapi.com/parser?ingr="+searchInput,
+      //"method": "GET",
+      //"headers": {
+        //"x-rapidapi-host": "edamam-food-and-grocery-database.p.rapidapi.com",
+        //"x-rapidapi-key": "ed79f9f0b7msh65c44d70d45c322p1f8f63jsne93f7593dc3d"
+      //}
+    //}
+  
+    //$.ajax(settings).done(function (response) {
+      //console.log(response);
+    //});
   });
