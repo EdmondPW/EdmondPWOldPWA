53dc17414253fca93f41f42afed0d79dad899340
EdmondPW <64176781+EdmondPW@users.noreply.github.com>
2020-06-24 18:56:18 +0700

    Update recepieSearch.js

--- src/js/recepieSearch.js
+++ src/js/recepieSearch.js
@@ -2,20 +2,33 @@ $( "#inputSearch" ).click(function() {
     var searchInput = document.getElementById("Search_Recipe").value;
     alert(searchInput);
     
-    var settings = {
-        "async": true,
-        "crossDomain": true,
-        "url": "https://edamam-recipe-search.p.rapidapi.com/search?q="+searchInput,
-        "method": "GET",
-        "headers": {
-            "x-rapidapi-host": "edamam-recipe-search.p.rapidapi.com",
-            "x-rapidapi-key": "ed79f9f0b7msh65c44d70d45c322p1f8f63jsne93f7593dc3d"
-        }
-    }
+    fetch("https://edamam-recipe-search.p.rapidapi.com/search?q="+searchInput, {
+            "method": "GET",
+            "headers": {
+                "x-rapidapi-host": "edamam-recipe-search.p.rapidapi.com",
+                "x-rapidapi-key": "ed79f9f0b7msh65c44d70d45c322p1f8f63jsne93f7593dc3d"
+            }
+        })
+        .then(response => {
+            console.log(response);
+        })
+        .catch(err => {
+            console.log(err);
+        });
+//     var settings = {
+//         "async": true,
+//         "crossDomain": true,
+//         "url": "https://edamam-recipe-search.p.rapidapi.com/search?q="+searchInput,
+//         "method": "GET",
+//         "headers": {
+//             "x-rapidapi-host": "edamam-recipe-search.p.rapidapi.com",
+//             "x-rapidapi-key": "ed79f9f0b7msh65c44d70d45c322p1f8f63jsne93f7593dc3d"
+//         }
+//     }
 
-    $.ajax(settings).done(function (response) {
-        console.log(response.hits);
-    });
+//     $.ajax(settings).done(function (response) {
+//         console.log(response.hits);
+//     });
     //var settings = {
       //"async": true,
       //"crossDomain": true,
