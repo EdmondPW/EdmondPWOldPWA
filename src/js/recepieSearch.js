6b2cf88627acc828399aa8bad9204f0219987acd
jeremyangga <62837186+jeremyangga@users.noreply.github.com>
2020-06-25 03:45:47 +0700

    Update recepieSearch.js

--- src/js/recepieSearch.js
+++ src/js/recepieSearch.js
@@ -1,42 +1,105 @@
 $( "#inputSearch" ).click(function() {
-    var searchInput = document.getElementById("Search_Recipe").value;
-    var calories=[];
+    	var simpan_arr = new Array();
+	var searchInput='';
+	var calories=[];
     var images=[];
     var ingredients=[];
     var totaltime=[];
     var label=[];
-    var totali=1;
-    var totalj=1;
-    alert(searchInput);
-    var show=document.getElementById('resep');
-    var div2=document.getElementById('j');
-    var bum=document.getElementById('bumbu');
-	show.innerHTML="";
-    fetch("https://edamam-recipe-search.p.rapidapi.com/search?q="+searchInput, {
-			"method": "GET",
-			"headers": {
-				"x-rapidapi-host": "edamam-recipe-search.p.rapidapi.com",
-				"x-rapidapi-key": "e737ff4f0fmsha2a34344d4ba3abp1f5b7ajsn2dff9f48a3bb"
-			}
-		})
-        .then(response => response.json()).then(function(data){
-        	for(i in data.hits){
-        	 show.innerHTML += " <div class='card mb-3' style='width: 18rem;'> <img id='img' class='card-img-top' src='"+data.hits[i].recipe.image+"'><div class='card-block'> <h4 class='card-title'>"+data.hits[i].recipe.label+"</h4> <p class='card-text'> Kalori: "+data.hits[i].recipe.calories+" </p><a href='#' id='save"+i+"' class='btn btn-primary btn-lg btn-block'>Save Recipe</a> </div> </div>";
-        		for(j in data.hits[i].recipe.ingredientLines){
-        		console.log("Bumbu: "+data.hits[i].recipe.ingredientLines[j]);
-        		data.hits[i].recipe.ingredientLines[j]
-        		
-        		}
-        		calories.push(data.hits[i].recipe.calories)
-        		images.push(data.hits[i].recipe.image);
-        		totaltime.push(data.hits[i].recipe.totalTime);
-        		label.push(data.hits[i].recipe.label);
-        		
-        	}
-        });
-        
-       
-        console.log(label);
+    var totali=0;
+    var totalj=[];
+	function input() {
+		totali=0;
+		totalj=[];
+		searchInput='';
+		calories=[];
+	    images=[];
+	    ingredients=[];
+	    totaltime=[];
+	    label=[];
+	    searchInput = document.getElementById("Search_Recipe").value;
+	    alert(searchInput);
+	    var show=document.getElementById('resep');
+	    var imageresep=document.getElementById('imageresep');
+	    var namaresep=document.getElementById('namaresep');
+	    show.innerHTML="";
+	    fetch("https://edamam-recipe-search.p.rapidapi.com/search?q="+searchInput, {
+				"method": "GET",
+				"headers": {
+					"x-rapidapi-host": "edamam-recipe-search.p.rapidapi.com",
+					"x-rapidapi-key": "e737ff4f0fmsha2a34344d4ba3abp1f5b7ajsn2dff9f48a3bb"
+				}
+			})
+	        .then(response => response.json()).then(function(data){
+	        	 for(i in data.hits){
+	        		totali=totali+1;
+	        		console.log(i);
+	        		var countj=0;
+	        		var temp_ing=[];
+	        		//simpan_arr.push(i);
+	        		//console.log(total);
+	        		//ingredients.push(data.hits[i].recipe.ingredientLines);
+	        		//console.log(i);
+	        		 show.innerHTML += " <div class='card mb-3' style='width: 18rem;'> <img id='img' class='card-img-top' src='"+data.hits[i].recipe.image+"'<div class='card-block'> <h4 class='card-title'>"+data.hits[i].recipe.label+"</h4> <p class='card-text'> Kalori: "+data.hits[i].recipe.calories+" </p><button onclick='save("+i+")' id='save"+i+"' value='"+i+"' class='btn btn-primary' style='align: center'>Save Recipe</button> </div> </div>";
+	        		for(j in data.hits[i].recipe.ingredientLines){
+	        			countj=countj+1;
+	        			console.log("Bumbu: "+data.hits[i].recipe.ingredientLines[j]);
+	        			temp_ing.push(data.hits[i].recipe.ingredientLines[j]);
+	        		}
+	        		ingredients.push(temp_ing);
+	        		totalj.push(countj);
+	        		calories.push(data.hits[i].recipe.calories)
+	        		images.push(data.hits[i].recipe.image);
+	        		totaltime.push(data.hits[i].recipe.totalTime);
+	        		label.push(data.hits[i].recipe.label);
+	        		 
+	        	}
+	        	//simpar_arr=data.hits;
+	        	console.log("Total = "+totali);
+	        	console.log(ingredients);
+	        	
+	        });
+	        
+	        //console.log(ingredients);
+  	}
+  function save(idx){
+  	console.log("Total I: "+totali);
+  	console.log("Total J: "+totalj);
+  	var index=document.getElementById("save"+idx).value;
+  	var sizej=totalj[index];
+  	console.log("index: "+index);
+  	console.log("sizej: "+sizej);
+  	console.log("Label index: "+label[index]);
+  	//console.log(ingredients[index]);
+  	var resepsave='';
+  	var ingredients_arr=ingredients[index];
+  	for(i in ingredients_arr){
+  		console.log(ingredients_arr[i]);
+  	}
+  	resepsave=ingredients_arr.toString();
+  	console.log(resepsave)
+  	var firebaseConfig = {
+    apiKey: "AIzaSyABQG0uk7MI0upKCVegOMF_3bjIwvxDQgM",
+    authDomain: "resepsaver.firebaseapp.com",
+    databaseURL: "https://resepsaver.firebaseio.com",
+    projectId: "resepsaver",
+    storageBucket: "resepsaver.appspot.com",
+    messagingSenderId: "104620766512",
+    appId: "1:104620766512:web:47b12acaedf67edff144f5"
+  };
+  firebase.initializeApp(firebaseConfig);
+  console.log(firebase);
+  	var database=firebase.database();  
+	var ref=database.ref('resepfavorit');
+	var data={
+	  	email: "edmond@mail.com",
+	  	label: label[index],
+	  	kalori: calories[index],
+	  	image: images[index],
+	  	resep: resepsave
+	  }
+	 ref.push(data);
+  }
 //     var searchInput = document.getElementById("Search_Recipe").value;
 //     alert(searchInput);
     
